b99a7906661f2b7b517cbc57ad3a9c75c788418b
Kevin Jolley <jolley427@gmail.comm>
2023-09-08 15:03:37 -0400

    something

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,11 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    @favorite = Favorite.new(
-      # user_id = params[id: current_user.id],
-      movie_id = params[:id],
-    )
+    @favorite = Favorite.find_or_create_by(user_id: current_user, movie_id: params[:id])
     if @favorite.save
       render json: @favorite
     else
-      render json: { errors: @favorite.errors.full_messages }, status: :bad_request
-    end
+      render json: { error: @favorite.errors.full_messages }, status: :bad_request
+    end    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :favorites
-  has_many :movies, through :favorites
+  has_many :movies, through: :favorites
 end
