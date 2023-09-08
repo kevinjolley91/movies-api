990c5a25b3a1561ee20ac5ae6b82cd1066f750b5
Kevin Jolley <jolley427@gmail.comm>
2023-09-08 16:42:46 -0400

    no idea

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,10 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    @favorite = Favorite.find_or_create_by(user_id: current_user, movie_id: params[:id])
+    @favorite = Favorite.find_or_create_by(
+      user_id: current_user.id,
+      movie_id: params[:movie_id]
+    )
     if @favorite.save
       render json: @favorite
     else
