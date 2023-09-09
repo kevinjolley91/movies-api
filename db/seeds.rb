bcdcad32b2c28dcd0efdd8916d02f093a54b3537
Kevin Jolley <jolley427@gmail.comm>
2023-09-08 21:54:13 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,8 @@ Favorite.create!([
   {user_id: 1, movie_id: 1},
   {user_id: 2, movie_id: 2},
   {user_id: 3, movie_id: 3},
-  {user_id: 4, movie_id: 4}
+  {user_id: 4, movie_id: 4},
+  {user_id: 1, movie_id: 2},
+  {user_id: 1, movie_id: 3},
+  {user_id: 1, movie_id: 4}
 ])
