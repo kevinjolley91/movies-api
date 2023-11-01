881538fba3a9c0ee027e8000721298713336004a
Kevin Jolley <jolley427@gmail.comm>
2023-11-01 13:34:43 -0400

    updates controller with search function

--- app/controllers/movies_lookup_controller.rb
+++ app/controllers/movies_lookup_controller.rb
@@ -20,4 +20,25 @@ class MoviesLookupController < ApplicationController
       render json: { error: "An error occurred: #{e.message}" }, status: :unprocessable_entity
     end
   end
+
+  def search
+    query = params[:query]
+    begin
+      url = URI.parse("https://api.themoviedb.org/3/search/movie?query=#{query}&api_key=#{ENV["MOVIE_API_KEY"]}")
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        http.request(req)
+      end
+
+      if res.is_a?(Net::HTTPSuccess)
+        @movies = JSON.parse(res.body)
+        @movies["results"].sort_by! { |movie| movie["title"] }
+        render json: @movies
+      else
+        render json: { error: "API request failed with status code: #{res.code}" }, status: :unprocessable_entity
+      end
+    rescue StandardError => e
+      render json: { error: "An error occurred: #{e.message}" }, status: :unprocessable_entity
+    end
+  end
 end
